9c92c37b21c33d82b3351b19e41feb4116e80a32
dev-n1ko <yukine05180607@gmail.com>
2026-01-22 00:08:32 +0900

    ユーザー設定関係

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,37 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :show]
+
+  
+  def index
+    @users = User.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to users_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(
+      :username,
+      :email,
+      :profile,
+      :image
+    )
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   root to: "home#index"
+  resources :users
 end
