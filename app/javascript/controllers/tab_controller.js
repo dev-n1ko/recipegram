bb74287f8fdf507c24fb6e044b471bbbc3d47d29
dev-n1ko <yukine05180607@gmail.com>
2026-01-29 18:35:22 +0900

    メモ機能のターゲット名変更

--- app/javascript/controllers/tab_controller.js
+++ app/javascript/controllers/tab_controller.js
@@ -1,17 +1,18 @@
 import { Controller } from "@hotwired/stimulus"
 
-export default class extends Controller {
-  static targets = ["tab", "content"]
+export default class Tabchenge extends Controller {
+  static targets = ["panel", "content"]
 
   switch(event) {
-    // console.log("event: ", event)
-
+    console.log("event: ", event)
+    
     const target = event.currentTarget.dataset.target
-    // console.log("target: ", target)
+    console.log("target: ", target)
+    console.log("target: ", event.currentTarget)
 
     // タブの active 切り替え
-    this.tabTargets.forEach(tab => {
-      tab.classList.toggle("is-active", tab.dataset.target === target)
+    this.panelTargets.forEach(panel => {
+      panel.classList.toggle("is-active", panel.dataset.target === target)
     })
 
     // コンテンツ切り替え
